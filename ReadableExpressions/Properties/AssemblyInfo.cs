6d17f60c38187067075b7044d9d97dfbeba7ce8e
Steve Wilkes <steve@agileobjects.co.uk>
2015-11-05 15:33:27 +0000

    Updating .NET framework target from 4.5 to 4.0

--- ReadableExpressions/Properties/AssemblyInfo.cs
+++ ReadableExpressions/Properties/AssemblyInfo.cs
@@ -8,5 +8,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2014")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
