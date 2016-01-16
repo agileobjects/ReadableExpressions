29138c428b0ec448147e1dac0f953d8ea261836c
Steve Wilkes <steve@agileobjects.co.uk>
2016-01-16 10:41:06 +0000

    Moving common assembly information into a shared file / Updated to v0.4

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Resources;
+using System.Reflection;
+
+[assembly: AssemblyCompany("AgileObjects Ltd")]
+[assembly: AssemblyProduct("AgileObjects.ReadableExpressions")]
+[assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2016")]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: AssemblyVersion("0.4.0.0")]
+[assembly: AssemblyFileVersion("0.4.0.0")]
--- ReadableExpressions/Properties/AssemblyInfo.cs
+++ ReadableExpressions/Properties/AssemblyInfo.cs
@@ -1,12 +1,4 @@
-﻿using System.Resources;
-using System.Reflection;
+﻿using System.Reflection;
 
 [assembly: AssemblyTitle("AgileObjects.ReadableExpressions")]
-[assembly: AssemblyCompany("AgileObjects Ltd")]
-[assembly: AssemblyProduct("AgileObjects.ReadableExpressions")]
 [assembly: AssemblyDescription("Provides an extension method to produce a readable string version of an expression tree.")]
-[assembly: AssemblyCopyright("Copyright © AgileObjects Ltd 2014")]
-[assembly: NeutralResourcesLanguage("en")]
-
-[assembly: AssemblyVersion("0.3.0.0")]
-[assembly: AssemblyFileVersion("0.3.0.0")]
