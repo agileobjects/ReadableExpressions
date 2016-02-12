cf8df8110a77a5a7f888c25d7522912a57fa9ff3
Steve Wilkes <steve@agileobjects.co.uk>
2016-02-11 19:40:01 -0800

    Adding VS2015 Expression DebuggerVisualizer

---
+++ ReadableExpressions.ExpressionVisualizer/ExpressionVisualizerObjectSource.cs
@@ -0,0 +1,17 @@
+﻿namespace AgileObjects.ReadableExpressions.ExpressionVisualizer
+{
+    using System.IO;
+    using System.Linq.Expressions;
+    using Microsoft.VisualStudio.DebuggerVisualizers;
+
+    public class ExpressionVisualizerObjectSource : VisualizerObjectSource
+    {
+        public override void GetData(object target, Stream outgoingData)
+        {
+            var expression = (Expression)target;
+            var readableExpression = expression.ToReadableString();
+
+            Serialize(outgoingData, readableExpression);
+        }
+    }
+}
