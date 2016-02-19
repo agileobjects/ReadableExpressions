16e725d4e4802f674175f6d6b82a6a0b6476244a
Steve Wilkes <steve@agileobjects.co.uk>
2016-02-18 19:20:28 -0800

    Test coverage for continue and return in loops

--- ReadableExpressions.UnitTests/WhenTranslatingLoops.cs
+++ ReadableExpressions.UnitTests/WhenTranslatingLoops.cs
@@ -50,5 +50,36 @@ while (true)
 }";
             Assert.AreEqual(EXPECTED.TrimStart(), translated);
         }
+
+        [TestMethod]
+        public void ShouldTranslateALoopWithAContinueStatement()
+        {
+            var intVariable = Expression.Variable(typeof(int), "i");
+            var intLessThanThree = Expression.LessThan(intVariable, Expression.Constant(3));
+            var incrementVariable = Expression.Increment(intVariable);
+            var continueLoop = Expression.Continue(Expression.Label());
+            var incrementAndContinue = Expression.Block(incrementVariable, continueLoop);
+            var ifLessThanThreeContinue = Expression.IfThen(intLessThanThree, incrementAndContinue);
+            Expression<Action> writeFinished = () => Console.Write("Finished!");
+            var returnFromLoop = Expression.Return(Expression.Label());
+            var loopBody = Expression.Block(ifLessThanThreeContinue, writeFinished.Body, returnFromLoop);
+            var loop = Expression.Loop(loopBody, returnFromLoop.Target, continueLoop.Target);
+
+            var translated = loop.ToReadableString();
+
+            const string EXPECTED = @"
+while (true)
+{
+    if (i < 3)
+    {
+        ++i;
+        continue;
+    }
+
+    Console.Write(""Finished!"");
+    return;
+}";
+            Assert.AreEqual(EXPECTED.TrimStart(), translated);
+        }
     }
 }
