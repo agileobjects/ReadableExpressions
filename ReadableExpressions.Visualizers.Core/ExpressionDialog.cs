1fd8acb7271bdac5620536ba134fc8b9b3c30356
Steve Wilkes <steve@agileobjects.co.uk>
2016-05-08 17:27:11 -0700

    Changing visualizer form to use a text box / Fixing visualizer layout and resizing

--- ReadableExpressions.Visualizers.Core/ExpressionDialog.cs
+++ ReadableExpressions.Visualizers.Core/ExpressionDialog.cs
@@ -8,29 +8,26 @@
     {
         public static readonly ExpressionDialog Instance = new ExpressionDialog();
 
-        private readonly Label _label;
+        private readonly TextBox _textBox;
         private readonly Size _maximumSize;
+        private readonly ToolStrip _toolbar;
+        private readonly int _titleBarHeight;
+        private bool _autoSize;
 
         private ExpressionDialog()
         {
             StartPosition = FormStartPosition.CenterScreen;
             MinimizeBox = false;
+            _maximumSize = GetDialogMaximumSize();
 
-            _label = new Label
-            {
-                Margin = new Padding(10, 15, 10, 12),
-                Font = new Font(FontFamily.GenericMonospace, 11.0f),
-                AutoSize = true
-            };
+            _textBox = AddTextBox();
+            _toolbar = AddToolbar();
 
-            _maximumSize = GetMaximumSize();
-
-            Controls.Add(_label);
-
-            AddToolbar();
+            var screenRectangle = RectangleToScreen(ClientRectangle);
+            _titleBarHeight = screenRectangle.Top - Top;
         }
 
-        private Size GetMaximumSize()
+        private Size GetDialogMaximumSize()
         {
             var screenSize = Screen.FromControl(this).Bounds.Size;
 
@@ -39,7 +36,30 @@
                 Convert.ToInt32(screenSize.Height * .8));
         }
 
-        private void AddToolbar()
+        private TextBox AddTextBox()
+        {
+            var textBox = new TextBox
+            {
+                Font = new Font(FontFamily.GenericMonospace, 11.0f),
+                Multiline = true,
+                ScrollBars = ScrollBars.Both,
+                ReadOnly = true
+            };
+
+            var textBoxPanel = new Panel
+            {
+                Dock = DockStyle.Fill
+            };
+
+            textBoxPanel.Controls.Add(textBox);
+            Controls.Add(textBoxPanel);
+
+            textBox.Resize += (sender, arg) => textBoxPanel.Size = ((Control)sender).Size;
+
+            return textBox;
+        }
+
+        private ToolStrip AddToolbar()
         {
             var copyButton = new ToolStripButton
             {
@@ -47,7 +67,7 @@
                 Text = "Copy",
             };
 
-            copyButton.Click += (sender, args) => Clipboard.SetText(_label.Text);
+            copyButton.Click += (sender, args) => Clipboard.SetText(_textBox.Text);
 
             var toolbar = new ToolStrip(copyButton)
             {
@@ -56,11 +76,11 @@
             };
 
             Controls.Add(toolbar);
+
+            return toolbar;
         }
 
-        public override bool AutoSize => true;
-
-        public override bool AutoScroll => true;
+        public override bool AutoSize => _autoSize;
 
         public override Size MaximumSize => _maximumSize;
 
@@ -77,10 +97,51 @@
             return base.ProcessDialogKey(keyData);
         }
 
+        protected override void OnResizeEnd(EventArgs e)
+        {
+            base.OnResizeEnd(e);
+
+            var newTextBoxSize = new Size(
+                Size.Width - 10,
+                Size.Height - _titleBarHeight - _toolbar.Height);
+
+            SetTextBoxSize(newTextBoxSize);
+        }
+
         public ExpressionDialog WithText(string expression)
         {
-            _label.Text = expression;
+            var textSize = TextRenderer.MeasureText(expression, _textBox.Font);
+
+            var textBoxSize = new Size(
+                textSize.Width + _textBox.Padding.Left + _textBox.Padding.Right + SystemInformation.VerticalScrollBarWidth + 10,
+                textSize.Height + _textBox.Padding.Top + _textBox.Padding.Bottom + _textBox.Font.Height);
+
+            SetTextBoxSize(textBoxSize);
+
+            _textBox.Text = expression;
+
             return this;
+        }
+
+        private void SetTextBoxSize(Size newSize)
+        {
+            EnableAutoSize();
+
+            _textBox.Size = newSize;
+
+            DisableAutoSize();
+        }
+
+        private void EnableAutoSize()
+        {
+            _autoSize =
+            _textBox.Parent.AutoSize = true;
+        }
+
+        private void DisableAutoSize()
+        {
+            _autoSize =
+            _textBox.Parent.AutoSize = false;
         }
     }
 }
