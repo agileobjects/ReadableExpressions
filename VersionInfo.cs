c5b6daca9f436fc9ca065b7715709220117bf4fb
Steve Wilkes <steve@agileobjects.co.uk>
2016-06-09 17:06:26 -0700

    Updating to v1.5.2

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.5.1")]
-[assembly: AssemblyFileVersion("1.5.1")]
+[assembly: AssemblyVersion("1.5.2")]
+[assembly: AssemblyFileVersion("1.5.2")]
