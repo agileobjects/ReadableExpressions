fa81d5ff52715a50e73a1f18e7362a818132ef3e
Steve Wilkes <steve@agileobjects.co.uk>
2016-10-05 12:36:04 +0100

    Test coverage for .NetStandard polyfills

--- ReadableExpressions/Properties/AssemblyInfo.cs
+++ ReadableExpressions/Properties/AssemblyInfo.cs
@@ -5,3 +5,4 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyDescription("Provides an extension method to produce a readable string version of an expression tree.")]
 
 [assembly: InternalsVisibleTo("AgileObjects.AgileMapper")]
+[assembly: InternalsVisibleTo("AgileObjects.ReadableExpressions.UnitTests.Polyfills")]
