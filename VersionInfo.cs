4b48d16ce9bdf432708e52d5a0ce0cc869f21eb2
Steve Wilkes <steve@agileobjects.co.uk>
2017-03-11 13:59:14 +0000

    Updating to v1.8.3

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.8.2")]
-[assembly: AssemblyFileVersion("1.8.2")]
+[assembly: AssemblyVersion("1.8.3")]
+[assembly: AssemblyFileVersion("1.8.3")]
