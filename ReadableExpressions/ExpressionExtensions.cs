73ab4d1bce584d0d6f994d1bb1757e5ea582afff
Dan Mann <dan@marsman.co.uk>
2017-07-10 09:28:12 +0100

    fix tab/space mess

--- ReadableExpressions/ExpressionExtensions.cs
+++ ReadableExpressions/ExpressionExtensions.cs
@@ -1,21 +1,21 @@
 ﻿namespace AgileObjects.ReadableExpressions
 {
-	using System.Linq.Expressions;
+    using System.Linq.Expressions;
 
-	/// <summary>
-	/// Provides the Expression translation extension method.
-	/// </summary>
-	public static class ExpressionExtensions
+    /// <summary>
+    /// Provides the Expression translation extension method.
+    /// </summary>
+    public static class ExpressionExtensions
     {
         private static readonly ExpressionTranslatorRegistry _translatorRegistry = new ExpressionTranslatorRegistry();
 
-		/// <summary>
-		/// Translates the given <paramref name="expression"/> to source-code string.
-		/// </summary>
-		/// <param name="expression">The Expression to translate.</param>
-		/// <param name="settings">Configuration options for the translation</param>
-		/// <returns>The translated <paramref name="expression"/>.</returns>
-		public static string ToReadableString(this Expression expression, ReadableStringSettings settings = null)
+        /// <summary>
+        /// Translates the given <paramref name="expression"/> to source-code string.
+        /// </summary>
+        /// <param name="expression">The Expression to translate.</param>
+        /// <param name="settings">Configuration options for the translation</param>
+        /// <returns>The translated <paramref name="expression"/>.</returns>
+        public static string ToReadableString(this Expression expression, ReadableStringSettings settings = null)
         {
             return _translatorRegistry
                 .Translate(expression, settings ?? new ReadableStringSettings())?
