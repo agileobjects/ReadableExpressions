0a45533c185f7984d186baf7bf3f1d112b159fe6
Steve Wilkes <steve@agileobjects.co.uk>
2017-07-11 20:16:58 +0100

    Removing old MSI installer projects

--- ReadableExpressions.Visualizers.Installer.Msi.Custom/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
