f999396a23c870ff9a4063aa28a8e4230213b022
Steve Wilkes <steve@agileobjects.co.uk>
2017-09-16 10:33:16 +0100

    Updating to v1.9.2

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.9.1")]
-[assembly: AssemblyFileVersion("1.9.1")]
+[assembly: AssemblyVersion("1.9.2")]
+[assembly: AssemblyFileVersion("1.9.2")]
