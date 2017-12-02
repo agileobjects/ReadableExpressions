5857c23731db1449ba1b1b460aabf6ca52f5d03a
Steve Wilkes <steve@agileobjects.co.uk>
2017-12-02 14:55:20 +0000

    Updating to ReadableExpressions v1.10

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.9.5")]
-[assembly: AssemblyFileVersion("1.9.5")]
+[assembly: AssemblyVersion("1.10.0")]
+[assembly: AssemblyFileVersion("1.10.0")]
