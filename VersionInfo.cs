2e5befc8b503232a4a91d42b78f73fe38cf9b757
Steve Wilkes <steve@agileobjects.co.uk>
2018-09-27 19:26:08 +0100

    Updating to v1.13

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("1.12.1")]
-[assembly: AssemblyFileVersion("1.12.1")]
+[assembly: AssemblyVersion("1.13.0")]
+[assembly: AssemblyFileVersion("1.13.0")]
