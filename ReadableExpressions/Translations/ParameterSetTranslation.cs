65ec06794f440002b87311702ec27cc8e0196345
Steve Wilkes <steve@agileobjects.co.uk>
2018-10-04 21:20:02 +0100

    Support for params array translation

--- ReadableExpressions/Translations/ParameterSetTranslation.cs
+++ ReadableExpressions/Translations/ParameterSetTranslation.cs
@@ -4,12 +4,14 @@ namespace AgileObjects.ReadableExpressions.Translations
 {
     using System.Collections.Generic;
     using System.Linq;
+    using System.Reflection;
 #if NET35
     using Microsoft.Scripting.Ast;
 #else
     using System.Linq.Expressions;
 #endif
     using Extensions;
+    using NetStandardPolyfills;
 
     internal class ParameterSetTranslation : ITranslation
     {
@@ -41,7 +43,9 @@ namespace AgileObjects.ReadableExpressions.Translations
             int parameterCount,
             ITranslationContext context)
         {
-            if (method?.IsExtensionMethod == true)
+            var methodProvided = method != null;
+
+            if (methodProvided && method.IsExtensionMethod)
             {
                 parameters = parameters.Skip(1);
                 --parameterCount;
@@ -56,12 +60,40 @@ namespace AgileObjects.ReadableExpressions.Translations
                 return;
             }
 
+            ParameterInfo[] methodParameters;
+
+            if (methodProvided)
+            {
+                methodParameters = method.GetParameters();
+                parameters = GetAllParameters(parameters, methodParameters);
+            }
+            else
+            {
+                methodParameters = null;
+            }
+
             var estimatedSize = 0;
 
             _parameterTranslations = parameters
-                .Project(p =>
+                .Project((p, index) =>
                 {
-                    var translation = context.GetTranslationFor(p);
+                    ITranslation translation;
+
+                    if (methodProvided)
+                    {
+                        // If a parameter is a params array then index will increase
+                        // past parameterCount, so adjust here:
+                        var parameterIndex = (ParameterCount != parameterCount)
+                            ? ParameterCount - parameterCount - index
+                            : index;
+
+                        // ReSharper disable once PossibleNullReferenceException
+                        translation = GetParameterTranslation(p, methodParameters[parameterIndex], context);
+                    }
+                    else
+                    {
+                        translation = context.GetTranslationFor(p);
+                    }
 
                     estimatedSize += translation.EstimatedSize;
 
@@ -73,9 +105,46 @@ namespace AgileObjects.ReadableExpressions.Translations
             EstimatedSize = estimatedSize + (ParameterCount * 2) + 4;
         }
 
+        private IEnumerable<Expression> GetAllParameters(
+            IEnumerable<Expression> parameters,
+            IList<ParameterInfo> methodParameters)
+        {
+            var i = 0;
+
+            foreach (var parameter in parameters)
+            {
+                if (methodParameters[i].IsParamsArray())
+                {
+                    var paramsArray = (NewArrayExpression)parameter;
+
+                    foreach (var paramsArrayValue in paramsArray.Expressions)
+                    {
+                        yield return paramsArrayValue;
+                        ++ParameterCount;
+                    }
+
+                    --ParameterCount;
+                    continue;
+                }
+
+                yield return parameter;
+                ++i;
+            }
+        }
+
+        private ITranslation GetParameterTranslation(
+            Expression parameter,
+            ParameterInfo info,
+            ITranslationContext context)
+        {
+            var translation = context.GetTranslationFor(parameter);
+
+            return translation;
+        }
+
         public int EstimatedSize { get; }
 
-        private int ParameterCount { get; }
+        private int ParameterCount { get; set; }
 
         public ParameterSetTranslation WithParentheses()
         {
@@ -98,14 +167,16 @@ namespace AgileObjects.ReadableExpressions.Translations
 
             context.WriteToTranslation('(');
 
-            for (var i = 0; i < ParameterCount; ++i)
+            for (var i = 0; ; ++i)
             {
                 _parameterTranslations[i].WriteTo(context);
 
-                if (i < ParameterCount - 1)
+                if (i == (ParameterCount - 1))
                 {
-                    context.WriteToTranslation(", ");
+                    break;
                 }
+
+                context.WriteToTranslation(", ");
             }
 
             context.WriteToTranslation(')');
