6eef2c9e64421680e0835d3283017c05227cec7d
Steve Wilkes <steve@agileobjects.co.uk>
2019-07-07 14:02:12 +0100

    Updating to v2.3.2 / v2.4

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -1,4 +1,4 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("2.3.1")]
-[assembly: AssemblyFileVersion("2.3.1")]
+[assembly: AssemblyVersion("2.3.2")]
+[assembly: AssemblyFileVersion("2.3.2")]
