7e2e37d99693953cdeeb459b77d48229dead6677
Steve Wilkes <steve@agileobjects.co.uk>
2020-01-13 18:48:44 +0000

    Fixing variable name generation for nested, generic types, re: issue #48

---
+++ ReadableExpressions/Extensions/PublicTypeExtensions.GetFriendlyName.cs
@@ -0,0 +1,163 @@
+﻿namespace AgileObjects.ReadableExpressions.Extensions
+{
+    using System;
+    using NetStandardPolyfills;
+    using Translations;
+    using static ExpressionExtensions;
+
+    /// <summary>
+    /// Provides a set of static extension methods for type information.
+    /// </summary>
+    public static partial class PublicTypeExtensions
+    {
+        /// <summary>
+        /// Returns a friendly, readable version of the name of the given <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">The type for which to retrieve a friendly, readable name.</param>
+        /// <param name="configuration">The configuration to use for the variable naming, if required.</param>
+        /// <returns>A friendly, readable version of the name of the given <paramref name="type"/>.</returns>
+        public static string GetFriendlyName(this Type type, Func<TranslationSettings, TranslationSettings> configuration = null)
+            => GetFriendlyName(type, GetTranslationSettings(configuration));
+
+        internal static string GetFriendlyName(this Type type, TranslationSettings translationSettings)
+        {
+            var buffer = new TranslationBuffer((type.FullName ?? type.ToString()).Length);
+
+            buffer.WriteFriendlyName(type, translationSettings);
+
+            return buffer.GetContent();
+        }
+
+        internal static void WriteFriendlyName(this TranslationBuffer buffer, Type type)
+            => WriteFriendlyName(buffer, type, TranslationSettings.Default);
+
+        internal static void WriteFriendlyName(
+            this TranslationBuffer buffer,
+            Type type,
+            TranslationSettings settings)
+        {
+            if (type.FullName == null)
+            {
+                if (type.IsGenericType())
+                {
+                    // A generic open generic parameter Type:
+                    buffer.WriteGenericTypeName(type, settings);
+                }
+                else
+                {
+                    // An open generic parameter Type:
+                    buffer.WriteToTranslation(type.Name);
+                }
+
+                return;
+            }
+
+            if (type.IsArray)
+            {
+                buffer.WriteFriendlyName(type.GetElementType(), settings);
+                buffer.WriteToTranslation("[]");
+                return;
+            }
+
+            if (!type.IsGenericType())
+            {
+                var substitutedTypeName = type.GetSubstitutionOrNull();
+
+                if (type.IsNested)
+                {
+                    buffer.WriteFriendlyName(type.DeclaringType, settings);
+                    buffer.WriteToTranslation('.');
+                    buffer.WriteToTranslation(substitutedTypeName ?? type.Name);
+                    return;
+                }
+
+                if (substitutedTypeName != null)
+                {
+                    buffer.WriteToTranslation(substitutedTypeName);
+                    return;
+                }
+
+                buffer.WriteTypeNamespaceIfRequired(type, settings);
+                buffer.WriteToTranslation(type.Name);
+                return;
+            }
+
+            Type underlyingNullableType;
+
+            if ((underlyingNullableType = Nullable.GetUnderlyingType(type)) == null)
+            {
+                buffer.WriteGenericTypeName(type, settings);
+                return;
+            }
+
+            buffer.WriteFriendlyName(underlyingNullableType, settings);
+            buffer.WriteToTranslation('?');
+        }
+
+        private static void WriteTypeNamespaceIfRequired(
+            this TranslationBuffer buffer,
+            Type type,
+            TranslationSettings settings)
+        {
+            if (!settings.FullyQualifyTypeNames || (type.Namespace == null))
+            {
+                return;
+            }
+
+            buffer.WriteToTranslation(type.Namespace);
+            buffer.WriteToTranslation('.');
+        }
+
+        private static void WriteGenericTypeName(
+            this TranslationBuffer buffer,
+            Type genericType,
+            TranslationSettings settings)
+        {
+            new GenericTypeNameWriter(buffer, settings).WriteGenericTypeName(genericType);
+        }
+
+        private class GenericTypeNameWriter : GenericTypeNameWriterBase
+        {
+            private readonly TranslationBuffer _buffer;
+            private readonly TranslationSettings _settings;
+
+            public GenericTypeNameWriter(TranslationBuffer buffer, TranslationSettings settings)
+            {
+                _buffer = buffer;
+                _settings = settings;
+            }
+
+            protected override void WriteTypeName(string typeName)
+                => _buffer.WriteToTranslation(typeName);
+
+            protected override bool TryWriteCustomAnonTypeName(Type anonType)
+            {
+                if (_settings.AnonymousTypeNameFactory == null)
+                {
+                    return base.TryWriteCustomAnonTypeName(anonType);
+                }
+
+                _buffer.WriteToTranslation(_settings.AnonymousTypeNameFactory.Invoke(anonType));
+                return true;
+            }
+
+            protected override void WriteTypeArgumentNamePrefix()
+                => _buffer.WriteToTranslation('<');
+
+            protected override void WriteTypeName(Type type)
+                => _buffer.WriteFriendlyName(type, _settings);
+
+            protected override void WriteTypeArgumentNameSeparator()
+                => _buffer.WriteToTranslation(", ");
+
+            protected override void WriteTypeArgumentNameSuffix()
+                => _buffer.WriteToTranslation('>');
+
+            protected override void WriteTypeNamePrefix(Type genericType)
+                => _buffer.WriteTypeNamespaceIfRequired(genericType, _settings);
+
+            protected override void WriteNestedTypeNamesSeparator()
+                => _buffer.WriteToTranslation('.');
+        }
+    }
+}
---
+++ ReadableExpressions/Extensions/PublicTypeExtensions.GetVariableName.cs
@@ -0,0 +1,125 @@
+﻿namespace AgileObjects.ReadableExpressions.Extensions
+{
+    using System;
+    using NetStandardPolyfills;
+    using static ExpressionExtensions;
+
+    public static partial class PublicTypeExtensions
+    {
+        /// <summary>
+        /// Retrieves a camel-case variable name for a variable of this <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">The Type for which to retrieve the variable name.</param>
+        /// <param name="configuration">The configuration to use for the variable naming, if required.</param>
+        /// <returns>A camel-case variable name for a variable of this <paramref name="type"/>.</returns>
+        public static string GetVariableNameInCamelCase(this Type type, Func<TranslationSettings, TranslationSettings> configuration = null)
+            => GetVariableNameInCamelCase(type, GetTranslationSettings(configuration));
+
+        internal static string GetVariableNameInCamelCase(this Type type, TranslationSettings settings)
+            => GetVariableName(type, settings).ToCamelCase();
+
+        /// <summary>
+        /// Retrieves a pascal-case variable name for a variable of this <paramref name="type"/>.
+        /// </summary>
+        /// <param name="type">The Type for which to retrieve the variable name.</param>
+        /// <param name="configuration">The configuration to use for the variable naming, if required.</param>
+        /// <returns>A pascal-case variable name for a variable of this <paramref name="type"/>.</returns>
+        public static string GetVariableNameInPascalCase(this Type type, Func<TranslationSettings, TranslationSettings> configuration = null)
+            => GetVariableNameInPascalCase(type, GetTranslationSettings(configuration));
+
+        private static string GetVariableNameInPascalCase(this Type type, TranslationSettings settings)
+            => GetVariableName(type, settings).ToPascalCase();
+
+        private static string GetVariableName(Type type, TranslationSettings settings)
+        {
+            if (type.IsArray)
+            {
+                return GetVariableName(type.GetElementType(), settings) + "Array";
+            }
+
+            var typeIsEnumerable = type.IsEnumerable();
+            var typeIsDictionary = typeIsEnumerable && type.IsDictionary();
+            var namingType = (typeIsEnumerable && !typeIsDictionary) ? type.GetEnumerableElementType() : type;
+            var variableName = GetBaseVariableName(namingType, settings);
+
+            if (namingType.IsInterface() && variableName.StartsWith('I'))
+            {
+                variableName = variableName.Substring(1);
+            }
+
+            if (namingType.IsGenericType())
+            {
+                variableName = GetGenericTypeVariableName(variableName, namingType, settings);
+            }
+
+            variableName = RemoveLeadingNonAlphaNumerics(variableName);
+
+            return (typeIsDictionary || !typeIsEnumerable) ? variableName : variableName.Pluralise();
+        }
+
+        private static string GetBaseVariableName(Type namingType, TranslationSettings translationSettings)
+            => namingType.IsPrimitive() ? namingType.GetFriendlyName(translationSettings) : namingType.Name;
+
+        private static string GetGenericTypeVariableName(
+            string variableName,
+            Type namingType,
+            TranslationSettings settings)
+        {
+            var nonNullableType = namingType.GetNonNullableType();
+            var genericTypeArguments = namingType.GetGenericTypeArguments();
+
+            if (nonNullableType != namingType)
+            {
+                return "nullable" + genericTypeArguments[0].GetVariableNameInPascalCase(settings);
+            }
+
+            var writer = new GenericVariableNameWriter(settings);
+            writer.WriteGenericTypeName(namingType);
+            
+            return writer.TypeName;
+        }
+
+        private static string RemoveLeadingNonAlphaNumerics(string value)
+        {
+            // Anonymous types start with non-alphanumeric characters
+            while (!char.IsLetterOrDigit(value, 0))
+            {
+                value = value.Substring(1);
+            }
+
+            return value;
+        }
+
+        private class GenericVariableNameWriter : GenericTypeNameWriterBase
+        {
+            private readonly TranslationSettings _settings;
+
+            public GenericVariableNameWriter(TranslationSettings settings)
+            {
+                _settings = settings;
+                TypeName = string.Empty;
+            }
+
+            public string TypeName { get; private set; }
+
+            protected override void WriteTypeName(string typeName)
+                => TypeName += typeName;
+
+            protected override void WriteTypeArgumentNamePrefix()
+                => TypeName += "_";
+
+            protected override void WriteTypeName(Type type)
+                => TypeName += type.GetVariableNameInPascalCase(_settings);
+
+            protected override void WriteTypeArgumentNameSeparator()
+                => TypeName += "_";
+
+            protected override void WriteTypeArgumentNameSuffix()
+            {
+            }
+
+            protected override void WriteNestedTypeNamesSeparator()
+                => TypeName += "__";
+        }
+    }
+}
