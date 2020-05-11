02ef8f46905d2554060d909bbb749b828f9f0544
Steve Wilkes <steve@agileobjects.co.uk>
2020-05-11 14:23:42 +0100

    Updating visualizers version number

--- ReadableExpressions.Visualizers.Core/CommonVisualizerAssemblyInfo.cs
+++ ReadableExpressions.Visualizers.Core/CommonVisualizerAssemblyInfo.cs
@@ -9,5 +9,5 @@ using System.Reflection;
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyVersion("3.1.0")]
-[assembly: AssemblyFileVersion("3.1.0")]
+[assembly: AssemblyVersion("3.2.0")]
+[assembly: AssemblyFileVersion("3.2.0")]
