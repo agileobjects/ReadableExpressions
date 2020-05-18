803e300cd72cd8d6503a0227762496fdcee86cad
Steve Wilkes <steve@agileobjects.co.uk>
2020-05-18 20:13:53 +0100

    v3.2..1 Visualizers and installer

--- ReadableExpressions.Visualizers.Core/CommonVisualizerAssemblyInfo.cs
+++ ReadableExpressions.Visualizers.Core/CommonVisualizerAssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("3.2.0")]
-[assembly: AssemblyFileVersion("3.2.0")]
+[assembly: AssemblyVersion("3.2.1")]
+[assembly: AssemblyFileVersion("3.2.1")]
