46f9aca48eee4391f0679b407f4127272de601d3
Steve Wilkes <steve@agileobjects.co.uk>
2020-05-21 13:36:38 +0100

    Removing NetCore projects / Adding v3.2.2 installer and visualizers

--- ReadableExpressions.Visualizers.Installer.Custom/VisualizerInstaller.cs
+++ ReadableExpressions.Visualizers.Installer.Custom/VisualizerInstaller.cs
@@ -1,9 +1,7 @@
 ﻿namespace AgileObjects.ReadableExpressions.Visualizers.Installer.Custom
 {
     using System;
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
     using Microsoft.Win32;
     using static System.StringComparison;
 
@@ -12,7 +10,7 @@
         private readonly Action<string> _logger;
         private readonly VsixManifestInstaller _vsixManifestInstaller;
         private readonly VisualizerAssembly _visualizerAssembly;
-        private readonly IList<string> _installDirectories;
+        private readonly string _visualizersDirectory;
 
         public VisualizerInstaller(
             Action<string> logger,
@@ -35,68 +33,30 @@
             var indexOfIde = vsInstallDirectory.IndexOf("IDE", OrdinalIgnoreCase);
             var pathToCommon7 = vsInstallDirectory.Substring(0, indexOfIde);
 
-            var rootInstallDirectory = Path.Combine(pathToCommon7, "Packages", "Debugger", "Visualizers");
-
-            if (visualizerAssembly.IsNetCore)
-            {
-                VsId += " (.NET Core)";
-                rootInstallDirectory = Path.Combine(rootInstallDirectory, "netcoreapp");
-            }
-
-            _installDirectories = new List<string> { rootInstallDirectory };
-
-            if (visualizerAssembly.IsNetStandard && Directory.Exists(rootInstallDirectory))
-            {
-                var netStandardSubDirectories = Directory
-                    .EnumerateDirectories(rootInstallDirectory)
-                    .Select(path => new DirectoryInfo(path))
-                    .Where(dir => dir.Name.StartsWith("netstandard", OrdinalIgnoreCase));
-
-                foreach (var netStandardSubDirectory in netStandardSubDirectories)
-                {
-                    _installDirectories.Add(netStandardSubDirectory.FullName);
-                }
-            }
+            _visualizersDirectory = Path.Combine(pathToCommon7, "Packages", "Debugger", "Visualizers");
         }
 
-        public string ResourceName => _visualizerAssembly.ResourceName;
+        public string ResourceName => _visualizerAssembly.VisualizerResourceName;
 
         public string VsId { get; }
 
-        private IEnumerable<string> GetInstallPaths()
-        {
-            return _installDirectories.Select(dir => Path
-                .Combine(dir, _visualizerAssembly.ResourceFileName));
-        }
-
         public void Install()
         {
-            foreach (var installPath in GetInstallPaths())
+            // ReSharper disable once AssignNullToNotNullAttribute
+            if (!Directory.Exists(_visualizersDirectory))
             {
-                // ReSharper disable once AssignNullToNotNullAttribute
-                if (!Directory.Exists(Path.GetDirectoryName(installPath)))
-                {
-                    Log("Skipping as directory does not exist: " + installPath);
-                    return;
-                }
-
-                _visualizerAssembly.WriteTo(installPath);
+                Log("Skipping as directory does not exist: " + _visualizersDirectory);
+                return;
             }
 
+            _visualizerAssembly.Install(_visualizersDirectory);
             _vsixManifestInstaller.Install();
         }
 
         public void Uninstall()
         {
             _vsixManifestInstaller.Uninstall();
-
-            foreach (var installPath in GetInstallPaths())
-            {
-                if (File.Exists(installPath))
-                {
-                    File.Delete(installPath);
-                }
-            }
+            _visualizerAssembly.Uninstall(_visualizersDirectory);
         }
 
         private void Log(string message) => _logger.Invoke(message);
