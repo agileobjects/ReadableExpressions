3f858940da9068e9d4e36c455e7802fa873785ae
Steve Wilkes <steve@agileobjects.co.uk>
2020-06-17 08:21:15 +0100

    Custom, data-bound copy button

---
+++ ReadableExpressions.Visualizers.Dialog.Wpf/Controls/CopyButton.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace AgileObjects.ReadableExpressions.Visualizers.Dialog.Wpf.Controls
+{
+    using System.Windows;
+
+    public partial class CopyButton
+    {
+        public CopyButton()
+        {
+            InitializeComponent();
+        }
+
+        public string TranslationRaw
+        {
+            get => (string)GetValue(TranslationRawProperty);
+            set => SetValue(TranslationRawProperty, value);
+        }
+
+        public static readonly DependencyProperty TranslationRawProperty =
+            DependencyProperty.RegisterAttached(
+                nameof(TranslationRaw),
+                typeof(string),
+                typeof(CopyButton));
+
+        public void CopyToClipboard(object sender, RoutedEventArgs e)
+        {
+            var copyButton = (CopyButton)sender;
+            var unformatted = copyButton.TranslationRaw;
+
+            Clipboard.SetText(unformatted);
+        }
+    }
+}
