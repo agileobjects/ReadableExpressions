f7b441f1eb7935d1e7c39f6ed449a75ca4f9e0bf
Steve Wilkes <SESA630636@se.com>
2024-09-22 12:56:39 +0100

    Visualizers v4.7.1

--- src/Visualizers/CommonVisualizerAssemblyInfo.cs
+++ src/Visualizers/CommonVisualizerAssemblyInfo.cs
@@ -4,5 +4,5 @@
 [assembly: AssemblyCompany("AgileObjects")]
 [assembly: AssemblyProduct("ReadableExpressions.Visualizers")]
 
-[assembly: AssemblyVersion("4.7.0")]
-[assembly: AssemblyFileVersion("4.7.0")]
+[assembly: AssemblyVersion("4.7.1")]
+[assembly: AssemblyFileVersion("4.7.1")]
